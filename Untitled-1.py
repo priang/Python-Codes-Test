237338e396d6f238e33c0c35e312aca2a3c8e3ce
priang <29465306+priang@users.noreply.github.com>
2025-06-13 08:37:52 -0300

    Update Untitled-1.py
    alteradas as variaveis setX para conjuntoX, adicionado comentarios

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,12 +1,16 @@
 
-set1 = {1,0,3.5,"casa","d","e","f"}
+
+conjunto1 = {1,0,3.5,"casa","d","e","f"}  #set
 lista = ["a","b","c"]
 dicionario = {"d":123,"e":234}
 
-set2 = set(dicionario) | set(lista)
+conjunto2 = set(dicionario) | set(lista)
 
-set4 = set1.difference(set2)
-#ou (tem a mesma funcao)
-set4 = set1 - set2
+# usar difference ou o operador '-' tera o mesmo resultado
+# opcao 1
+conjunto3 = conjunto1.difference(conjunto2)
+# opcao 2
+conjunto4 = conjunto1 - conjunto2
 
-print(set4)
+print('conjunto 3', conjunto3)
+print('conjunto 4', conjunto4)
